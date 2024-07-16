b70ba0565280dd4acc7c9a1f5541a656a0881dea
elliot108 <123285230+elliot108@users.noreply.github.com>
2024-07-16 20:23:10 +0630

    Formatted with ruff

--- calculator.py
+++ calculator.py
@@ -1,25 +1,39 @@
 import logging
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters,InlineQueryHandler, CallbackContext
 from uuid import uuid4
-import necessary_functions as myFunctions
 
-# to see when and why things don't work as expected 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+from telegram import (
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    Update,
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    InlineQueryHandler,
+    MessageHandler,
+    filters,
 )
 
-# start command 
+import necessary_functions as myFunctions
+
+# to see when and why things don't work as expected
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+# start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = "Welcome, I am here to assist you with Maths"
     await context.bot.send_message(update.effective_chat.id, message)
 
-# help command 
+
+# help command
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message = '''
+    message = """
                 I can help you calculate your math problems.
-                \nNot only simple math problems, you can also solve trigonometric problems. 
+                \nNot only simple math problems, you can also solve trigonometric problems.
                 \nYou can control me by sending the command or just send me your numbers!
                 \nYou can also solve a problem in any chat by calling @calculatorByElliott_bot!
                 \n/calulate - compute the mathematical expression that the user enters
@@ -28,52 +42,61 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 \nDivision but to retrieve remainder - '%'
                 \nDivision but to retrieve quotient - '//'
                 \nFor other operators, you can use normal signs. i.e. for addition, use '+'
-                '''
-            
+                """
+
     await context.bot.send_message(update.effective_chat.id, message)
-    
+
+
 # this funciton will be run when the user sends /calculate command
 async def command_calculate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    bot = context.bot 
+    bot = context.bot
 
-    input = str("".join(context.args)).replace(" ", "").lower() # removing all spaces in the input
+    input = (
+        str("".join(context.args)).replace(" ", "").lower()
+    )  # removing all spaces in the input
     input = input.replace("[]", "")
     response = myFunctions.calculate(input)
 
-    if type(response) == tuple:
+    if type(response) is tuple:
         input, result = response
-        response = "The answer of '"+ str(input) + "' is " + str(result)
-    
+        response = "The answer of '" + str(input) + "' is " + str(result)
+
     await bot.send_message(chat_id, response)
 
+
 # this will be run when the user mentions this bot's username
-async def inline_calculate(update:Update, context: ContextTypes.DEFAULT_TYPE):
+async def inline_calculate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.inline_query.query
-    input = str("".join(query)).replace(" ", "").lower() # removing all spaces in the input
+    input = (
+        str("".join(query)).replace(" ", "").lower()
+    )  # removing all spaces in the input
     after_calculating = myFunctions.calculate(input)
     # if the query could be evaluated, after_calculating should be a tuple
-    if type(after_calculating)==tuple:
-        result = after_calculating[1] # this will be the result of the user's math problem
+    if isinstance(after_calculating, tuple):
+        result = after_calculating[
+            1
+        ]  # this will be the result of the user's math problem
         message = result
     else:
         if query:
-            result = str(query) # the user's query will be reflected
+            result = str(query)  # the user's query will be reflected
         else:
             result = "NO INPUT"
         print(after_calculating)
         message = after_calculating
 
-    inline_response = [] 
+    inline_response = []
     inline_response.append(
         InlineQueryResultArticle(
-            id= str(uuid4()), #creating a random unique id
-            title= "Calculate", #to show above the text box
+            id=str(uuid4()),  # creating a random unique id
+            title="Calculate",  # to show above the text box
             input_message_content=InputTextMessageContent(result),
-            description = message
+            description=message,
         )
     )
     await context.bot.answer_inline_query(update.inline_query.id, inline_response)
+
 
 # thsi funciton will be run when the suer sends a message
 async def msg_calculate(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -84,46 +107,58 @@ async def msg_calculate(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     response = myFunctions.calculate(input)
 
-    if type(response) == tuple:
+    if isinstance(response, tuple):
         input, result = response
-        response = "The answer of '"+ str(input) + "' is " + str(result)
-    
+        response = "The answer of '" + str(input) + "' is " + str(result)
+
     await bot.send_message(chat_id, response)
 
-# unknown command handler 
-async def unknown(update=Update, context = ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id = update.effective_chat.id, text = "Sorry I do not understand this command")
+
+# unknown command handler
+async def unknown(update=Update, context=ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Sorry I do not understand this command"
+    )
+
 
 if __name__ == "__main__":
     # to initiate telegram bot
-    application = ApplicationBuilder().token("7490282882:AAGRs2KGK-NdUGZW5pmq-i-71BZzBDaMHes").build()
+    application = (
+        ApplicationBuilder()
+        .token("7490282882:AAGRs2KGK-NdUGZW5pmq-i-71BZzBDaMHes")
+        .build()
+    )
 
-    # start command handler 
+    # start command handler
     start_cm_handler = CommandHandler("start", start)
 
-    # help command handler 
+    # help command handler
     help_cm_handler = CommandHandler("help", help)
 
-    # calculate command handler 
+    # calculate command handler
     calculate_cm_handler = CommandHandler("calculate", command_calculate)
 
     # calculate inlinequery handler
     calculate_inlinequery_handler = InlineQueryHandler(inline_calculate)
 
-    # calculate message handler 
-    calculate_msg_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), msg_calculate)
+    # calculate message handler
+    calculate_msg_handler = MessageHandler(
+        filters.TEXT & (~filters.COMMAND), msg_calculate
+    )
 
-    # create unknown command handler 
+    # create unknown command handler
     unknown_cmd_handler = MessageHandler(filters.COMMAND, unknown)
 
     # adding all handlers
-    application.add_handlers([
-        start_cm_handler,
-        calculate_cm_handler, 
-        help_cm_handler,
-        calculate_inlinequery_handler,
-        calculate_msg_handler,
-        unknown_cmd_handler
-        ])
+    application.add_handlers(
+        [
+            start_cm_handler,
+            calculate_cm_handler,
+            help_cm_handler,
+            calculate_inlinequery_handler,
+            calculate_msg_handler,
+            unknown_cmd_handler,
+        ]
+    )
 
     application.run_polling()
--- necessary_functions.py
+++ necessary_functions.py
@@ -1,4 +1,6 @@
 import math
+
+
 # to check if the user's input contains alphabets or other characters that are not mathematical notations
 def contains_Alpha_Or_SpecCha(input):
     for c in input:
@@ -6,39 +8,49 @@ def contains_Alpha_Or_SpecCha(input):
             return True
     return False
 
-# to check if the user's mathematical expression includes trigonometry 
+
+# to check if the user's mathematical expression includes trigonometry
 def containsTrigoSigns(input: str):
-    if input.find("sin") or input.find("cos") or input.find("tan") or input.find("cot") or input.find("sec") or input.find("cos"):
+    if (
+        input.find("sin")
+        or input.find("cos")
+        or input.find("tan")
+        or input.find("cot")
+        or input.find("sec")
+        or input.find("cos")
+    ):
         return True
     return False
 
+
 # to compute Trigonometry functions
 def computeTrigo(triSign, angle):
-     if triSign == "sin":
-          return math.sin(angle)
-     elif triSign == "cos":
-          return math.cos(angle)
-     elif triSign == "tan":
-          return math.tan(angle)
-     elif triSign == "cot":
-          return math.atan(angle)
-     elif triSign == "sec":
-          return math.acos(angle)
-     elif triSign == "cosec":
-          return math.asin(angle)
+    if triSign == "sin":
+        return math.sin(angle)
+    elif triSign == "cos":
+        return math.cos(angle)
+    elif triSign == "tan":
+        return math.tan(angle)
+    elif triSign == "cot":
+        return math.atan(angle)
+    elif triSign == "sec":
+        return math.acos(angle)
+    elif triSign == "cosec":
+        return math.asin(angle)
+
 
 # a funciton to handle trogonometric expressions from user
 def calculateTrigo(input):
     print(input)
-    s = input # user's input will not be changed, only 's' will be changed later
+    s = input  # user's input will not be changed, only 's' will be changed later
     trigoSigns = ["sin", "cosec", "cos", "tan", "cot", "sec"]
-    isEnclosed = False # a flag to indicate if the degree is enclosed behind trigo sign
+    isEnclosed = False  # a flag to indicate if the degree is enclosed behind trigo sign
     # find each sign of trigo in the input
     for t in trigoSigns:
         f = s.find(t)
         while not f == -1:
             if not t == "cosec":
-                index = f+3
+                index = f + 3
             else:
                 index = f + 5
 
@@ -46,60 +58,64 @@ def calculateTrigo(input):
 
             # check if next character is a number
             if next_cha.isdigit():
-                    angle_measure = next_cha
-                    index += 1
+                angle_measure = next_cha
+                index += 1
             # check if the degree or angle behind trigo is enclosed in brackets
             elif next_cha == "(":
-                    isEnclosed = True
-                    angle_measure = s[index+1]
-                    print(angle_measure)
-                    index += 2
+                isEnclosed = True
+                angle_measure = s[index + 1]
+                print(angle_measure)
+                index += 2
             else:
-                print("no degree behind " + t) 
+                print("no degree behind " + t)
                 return s
-            
+
             # collecting the degree or angle measure described after trigo sign
             while index < len(s):
-                        next_cha = s[index]
-                        if next_cha.isdigit():
-                            angle_measure += next_cha #add subsequent numbers as angle to compute trigo function
-                            index += 1
-                            continue
+                next_cha = s[index]
+                if next_cha.isdigit():
+                    angle_measure += next_cha  # add subsequent numbers as angle to compute trigo function
+                    index += 1
+                    continue
 
-                        elif next_cha == ".":
-                            if next_cha.find("."):
-                                print("more than one decimal points in angle measurement behind " + t)
-                                return s
-                            angle_measure += next_cha
-                            index += 1
-                            continue
-                        else:
-                            break
-            
+                elif next_cha == ".":
+                    if next_cha.find("."):
+                        print(
+                            "more than one decimal points in angle measurement behind "
+                            + t
+                        )
+                        return s
+                    angle_measure += next_cha
+                    index += 1
+                    continue
+                else:
+                    break
+
             # enclosing trigo functions so it will be easier to compute
             # e.g. sin80 => (sin80) or sin(30) => (sin30)
             trigo = ""
             if isEnclosed:
-                 trigo = t + "("+ angle_measure + ")"
-                 s = s.replace(trigo, t+angle_measure)
-            trigo = t+angle_measure
+                trigo = t + "(" + angle_measure + ")"
+                s = s.replace(trigo, t + angle_measure)
+            trigo = t + angle_measure
             print(trigo)
-            s = s.replace(trigo, "("+trigo+")")
+            s = s.replace(trigo, "(" + trigo + ")")
             print(s)
 
             # computing trigo functions
-            angle_measure = eval(angle_measure) # from string to number
-            radian = math.radians(angle_measure) # from degree to radian 
+            angle_measure = eval(angle_measure)  # from string to number
+            radian = math.radians(angle_measure)  # from degree to radian
             trigo_value = computeTrigo(t, radian)
             s = s.replace(trigo, str(trigo_value))
 
-            index+= 2
+            index += 2
 
             if index < len(s):
                 f = s.find(t, index)
             else:
-                break                                   
-    return s # return 
+                break
+    return s  # return
+
 
 # this funciton returns the indeces of the same characters found in a string
 # return False if the character is not found
@@ -107,96 +123,107 @@ def find_and_return_index(s, f):
     isFound = s.find(f)
     index = []
     while int(isFound) >= 0:
-        index += isFound,
-        isFound = s.find(f, isFound+1, len(s)-1)
+        index += (isFound,)
+        isFound = s.find(f, isFound + 1, len(s) - 1)
     if index:
         return index
     else:
         return False
-    
-# this function returns a string that has replaced the characters 
+
+
+# this function returns a string that has replaced the characters
 # with the string user provided
 # 's' is a string that needs to be replaced
 # 'r' is a string with which the user wants to replace the character
 # '*i' is a tuple in which index numbers of characters that user wants to replace
-def replace_by_index(s: str,r:str, *i):
+def replace_by_index(s: str, r: str, *i):
     new_str = ""
     print(i)
-    if len(i)>1:        
-        new_str += s[:i[0]] + r
+    if len(i) > 1:
+        new_str += s[: i[0]] + r
         index = 1
         while index < len(i):
-            if i[index] == i[index-1]:
+            if i[index] == i[index - 1]:
                 index += 1
                 continue
-            new_str += s[i[index-1]+1:i[index]] + r
+            new_str += s[i[index - 1] + 1 : i[index]] + r
             index += 1
 
-        new_str += s.removeprefix(s[:i[len(i)-1]+1])
+        new_str += s.removeprefix(s[: i[len(i) - 1] + 1])
         return new_str
     else:
-        new_str += s[:i[0]] + r
-        new_str += s.removeprefix(s[:i[0]+1])
+        new_str += s[: i[0]] + r
+        new_str += s.removeprefix(s[: i[0] + 1])
         print(new_str)
         return new_str
 
+
 # this functions adds "*" between two brackets or between a number and a bracket
 def multiply_brackets(number_string):
-    #index numbers of "(" in the string
-    indeces_of_open_brackets = find_and_return_index(number_string[1:],"(")
+    # index numbers of "(" in the string
+    indeces_of_open_brackets = find_and_return_index(number_string[1:], "(")
     i = 0
     temp = indeces_of_open_brackets.copy()
     length = len(indeces_of_open_brackets)
-    while i <= length-1:
+    while i <= length - 1:
         # if the char before "(" is not a digit
         char_before_open_bracket = number_string[indeces_of_open_brackets[i]]
         if not char_before_open_bracket.isdigit():
-                temp.remove(indeces_of_open_brackets[i]) # "*" won't be added before "("
-        i+= 1
+            temp.remove(indeces_of_open_brackets[i])  # "*" won't be added before "("
+        i += 1
     indeces_of_open_brackets = temp
 
     if indeces_of_open_brackets:
         # adding "*" before "(" only if it should be added
-        number_string = number_string[0]+ replace_by_index(number_string[1:],"*(", *indeces_of_open_brackets)        
+        number_string = number_string[0] + replace_by_index(
+            number_string[1:], "*(", *indeces_of_open_brackets
+        )
 
-    #index numbers of ")" in the string
-    indeces_of_close_brackets = find_and_return_index(number_string[1:],")")
+    # index numbers of ")" in the string
+    indeces_of_close_brackets = find_and_return_index(number_string[1:], ")")
     i = 0
     temp = indeces_of_close_brackets.copy()
     length = len(indeces_of_close_brackets)
-    while i <= length-1:
+    while i <= length - 1:
         # if the char after ")" is not a digit
-        if indeces_of_close_brackets[i]+1 == len(number_string)-1:
-             temp.remove(indeces_of_close_brackets[i])
-             break
-        if not number_string[indeces_of_close_brackets[i]+2].isdigit():
-                temp.remove(indeces_of_close_brackets[i])
-        i+= 1
+        if indeces_of_close_brackets[i] + 1 == len(number_string) - 1:
+            temp.remove(indeces_of_close_brackets[i])
+            break
+        if not number_string[indeces_of_close_brackets[i] + 2].isdigit():
+            temp.remove(indeces_of_close_brackets[i])
+        i += 1
 
     indeces_of_close_brackets = temp
 
     if indeces_of_close_brackets:
         # adding "*" before "(" only if it should be added
-        number_string = number_string[0]+ replace_by_index(number_string[1:],")*", *indeces_of_close_brackets)        
+        number_string = number_string[0] + replace_by_index(
+            number_string[1:], ")*", *indeces_of_close_brackets
+        )
 
     return number_string
+
 
 # this function is used to take user's input as string and evaluate it
 # return a tuple (input and the result) if the input can be evaluated
 # return a message if the input cannot be evaluated
 def calculate(input):
-    if input:  
+    if input:
         if input.find("^"):
-            input = input.replace("^", "**") 
+            input = input.replace("^", "**")
         if input.find("÷"):
             input = input.replace("÷", "/")
         if input.find("×"):
             input = input.replace("×", "*")
 
-        if contains_Alpha_Or_SpecCha(input): #if the user's input contains alphabets or other characters that are not mathematical notations
-            if containsTrigoSigns(input): # if the input has trigo signs
+        if contains_Alpha_Or_SpecCha(
+            input
+        ):  # if the user's input contains alphabets or other characters that are not mathematical notations
+            if containsTrigoSigns(input):  # if the input has trigo signs
                 try:
-                    number_only = calculateTrigo(input) # calculate the trigo functions and store only numbers and operators
+                    number_only = calculateTrigo(
+                        input
+                    )  # calculate the trigo functions and store only numbers and operators
                 except ValueError:
                     error_msg = "Invalid input for trigo function. \nPlease check your trigo signs and degree measures."
                     return error_msg
@@ -205,15 +232,15 @@ def calculate(input):
                 error_msg = "Please enter numbers and operators only."
                 return error_msg
         else:
-            number_only = input # input has only numbers and operators
+            number_only = input  # input has only numbers and operators
 
         try:
             print(number_only)
-            result = eval(number_only) # evaluating the user's math problem 
-        except (SyntaxError,NameError,TypeError,SyntaxWarning) as e: 
+            result = eval(number_only)  # evaluating the user's math problem
+        except (SyntaxError, NameError, TypeError, SyntaxWarning) as e:
             print(e)
             # if the user's input cannot be evaluated
-            if type(e) == TypeError:
+            if isinstance(e, TypeError):
                 number_only = number_only.replace(")(", ")*(")
                 try:
                     print(number_only)
@@ -221,30 +248,31 @@ def calculate(input):
                     return number_only, result
                 except (TypeError, SyntaxError):
                     print(number_only)
-                    new_number_only= multiply_brackets(number_only)
+                    new_number_only = multiply_brackets(number_only)
                     try:
                         print(new_number_only)
-                        result = eval(new_number_only) # evaluating the user's math problem 
-                        return new_number_only,result
-                    except (SyntaxError,NameError,TypeError,SyntaxWarning) as e:
-                    # result = eval(number_only_new)
-                        error_msg = '''Please enter a meaningful mathematical expression.
+                        result = eval(
+                            new_number_only
+                        )  # evaluating the user's math problem
+                        return new_number_only, result
+                    except (SyntaxError, NameError, TypeError, SyntaxWarning) as e:
+                        # result = eval(number_only_new)
+                        error_msg = """Please enter a meaningful mathematical expression.
                             \nMake sure brackets are enclosed if you enter brackets.
-                            '''
+                            """
                         return error_msg
-                    
 
-            error_msg = '''Please enter a meaningful mathematical expression.
+            error_msg = """Please enter a meaningful mathematical expression.
                         \nMake sure there are at least two operands and one operator in the middle.
                         \nThere should be no operator alone at the end.
-                        \nAlphabets or words except trigo signs are not accepted. 
+                        \nAlphabets or words except trigo signs are not accepted.
                         \nRemember that I cannot understand some operators that may make sense to you. e.g, if you want to do division, use this '/' operator.
                         \nAnd please use '*' to multiply. i.e. sending (3)(9) will raise an error.
-                        '''
+                        """
             return error_msg
 
-        return input,result
-    
+        return input, result
+
     else:
         message = "Enter a mathematical expression. I will compute it and send the result to you."
         return message
